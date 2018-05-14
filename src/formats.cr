19041f00d1f2544ddcaf3935dea36280cb29e731
Sanqui <gsanky@gmail.com>
2018-05-14 21:48:56 +0200

    Add support for NES composite 2bpp
    (also adds nes_1bpp, which is redundant with Game Boy's.)

--- src/formats.cr
+++ src/formats.cr
@@ -5,6 +5,8 @@ module Dazzlie
         "gb_2bpp"      => Formats::Gb2Bpp,
         "gb_1bpp"      => Formats::Gb1Bpp,
         "gb_rows_2bpp" => Formats::GbRow2Bpp,
-        "gb_rows_1bpp" => Formats::GbRow1Bpp
+        "gb_rows_1bpp" => Formats::GbRow1Bpp,
+        "nes_2bpp"     => Formats::NES2Bpp,
+        "nes_1bpp"     => Formats::NES1Bpp,
     }
 end
